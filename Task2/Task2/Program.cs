26338a00f89df7e49106124ec715267b8db63849
Mysh <tipomysh.ru@mail.ru>
2024-12-08 21:25:42 +0300

    Commit on new branch

---
+++ Task2/Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for ( var i = 10; i <= 99; i += 5)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = -20;
+            do
+            {
+                Console.WriteLine(i);
+                i -= 20;
+            }
+            while (i >= -100);
+        }
+    }
+}
